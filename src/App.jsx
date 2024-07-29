de182bb15c5a3a1f7bbdfa5f11e99aa18e7ac7eb
DHANANJAY CHAUHAN <60539239+Dhan9500@users.noreply.github.com>
2024-07-29 11:21:41 +0000

    Scroll Fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Clock from "./components/Clock";
 
 function App() {
 	return (
-		<div className="h-screen w-screen container mx-auto flex justify-center items-center bg-gray-700 overflow-hidden select-none">
+		<div className="h-svh w-svw container mx-auto flex justify-center items-center bg-gray-700 overflow-hidden select-none">
 			<Clock />
 		</div>
 	);
