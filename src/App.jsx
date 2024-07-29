c3fbe93c1864760613a7143bc9db61bce6619298
DHANANJAY CHAUHAN <60539239+Dhan9500@users.noreply.github.com>
2024-07-29 11:18:20 +0000

    Fixed responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Clock from './components/Clock';
+import Clock from "./components/Clock";
 
 function App() {
 	return (
-		<div className='h-svh w-svw container mx-auto flex justify-center items-center bg-gray-700 overflow-hidden select-none'>
+		<div className="h-screen w-screen container mx-auto flex justify-center items-center bg-gray-700 overflow-hidden select-none">
 			<Clock />
 		</div>
 	);
--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,13 +1,44 @@
 /* eslint-disable no-unused-vars */
-import { UseContextData } from '../context/Context';
-import Analog from './Analog';
-import Day from './Day';
-import Time from './Time';
+import { UseContextData } from "../context/Context";
+import Analog from "./Analog";
+import Day from "./Day";
+import Time from "./Time";
 const Clock = () => {
 	const { clickToggle, toggle, loading, changeClock, clock } = UseContextData();
 	return (
-		<div className='h-[50%] w-[90%] bg-gray-700 flex relative justify-center items-center rounded-md shadow-2xl shadow-black'>
-			<div className=' shadow-2xl shadow-indigo-500 rounded-md '>
+		<div className="h-[350px] w-[300px]  bg-gray-700 flex flex-col gap-5 justify-center items-center rounded-md shadow-md shadow-black">
+			<div
+				className={` w-[245px] h-[40px]  ${clock ? "mb-14" : "mt-4"} flex justify-between items-center gap-2 overflow-hidden `}
+			>
+				<input
+					onChange={() => {
+						changeClock();
+					}}
+					type="checkbox"
+					className="toggle toggle-primary right-64 bottom-72"
+					checked={clock}
+				/>
+				<span
+					className={`label-text  absolute font-semibold  text-red-600 ${clock ? "text-[0.5rem] ml-1" : "text-[0.5rem] ml-5"}`}
+				>
+					{clock ? "Digital" : "Analog"}
+				</span>
+
+				<input
+					onChange={() => {
+						clickToggle();
+					}}
+					type="checkbox"
+					className={`toggle toggle-primary left-64 bottom-72 `}
+					checked={toggle}
+				/>
+				<span
+					className={`label-text absolute text-[0.54rem] font-semibold ${toggle ? "ml-[12.7rem]" : "ml-[13.8rem]"} text-red-600`}
+				>
+					{toggle ? "24H" : "12H"}
+				</span>
+			</div>
+			<div className=" shadow-lg shadow-indigo-500 rounded-md ">
 				{loading ? <></> : <></>}
 				{clock ? (
 					<>
@@ -18,33 +49,6 @@ const Clock = () => {
 					<Analog />
 				)}
 			</div>
-			<span
-				className={`label-text text-xs font-semibold absolute left-56 bottom-72 mb-1 text-red-600 ${
-					clock ? 'visible' : 'invisible'
-				}`}
-			>
-				{toggle ? '24H' : '12H'}
-			</span>
-			<input
-				onChange={() => {
-					clickToggle();
-				}}
-				type='checkbox'
-				className={`toggle toggle-primary absolute left-64 bottom-72 ${clock ? 'visible' : 'invisible'}`}
-				checked={toggle}
-			/>
-
-			<input
-				onChange={() => {
-					changeClock();
-				}}
-				type='checkbox'
-				className='toggle toggle-primary absolute right-64 bottom-72'
-				checked={clock}
-			/>
-			<span className={`label-text text-xs font-semibold absolute left-[75px] bottom-72 mb-1 text-red-600`}>
-				{clock ? 'Digital' : 'Analog'}
-			</span>
 		</div>
 	);
 };
